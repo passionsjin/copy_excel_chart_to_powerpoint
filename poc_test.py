0fbb97aee7cd36e6a7a66d8003fd4daf04ec4eae
passionsjin <dudwls251@naver.com>
2019-07-10 17:33:19 +0900

    ppt 파일 지정가능, 슬라이드 지정가능, 차트(객체) 크기-위치 지정가능

--- poc_test.py
+++ poc_test.py
@@ -7,13 +7,20 @@ import os
 # https://stackoverflow.com/questions/32639900/charts-from-excel-to-powerpoint-with-python
 xlApp = Dispatch('Excel.Application')
 pptApp = Dispatch('PowerPoint.Application')
-workbook = xlApp.Workbooks.Open('C:\\Users\\Park\\Desktop\\private_project\\POC_chart_copy_to_ppt\\data_chart.xlsx')
 
-presentation = pptApp.Presentations.Add(True)
+base_path = os.path.dirname(os.path.abspath(__file__))
+xl_path = os.path.join(base_path, 'data_chart.xlsx')
+ppt_path = os.path.join(base_path, 'target_ppt.pptx')
+
+workbook = xlApp.Workbooks.Open(xl_path)
+
+# presentation = pptApp.Presentations.Add(True)
+presentation = pptApp.Presentations.Open(ppt_path)
 xlApp.Sheets("Sheet1").Select()
 
 xlSheet1 = xlApp.Sheets(1)
 # for ws in workbook.Worksheets:
+count = 1
 for chart in xlSheet1.ChartObjects():
     print(chart.name)
     chart.Activate()
@@ -21,15 +28,21 @@ for chart in xlSheet1.ChartObjects():
     print(constants.__dicts__)
     # https://www.thespreadsheetguru.com/blog/2014/3/17/copy-paste-an-excel-range-into-powerpoint-with-vba
     # https://docs.microsoft.com/ko-kr/office/vba/api/PowerPoint.PpSlideLayout
-    Slide = presentation.Slides.Add(presentation.Slides.Count + 1, 12)
-    Slide.Shapes.PasteSpecial(11)
+    # https://docs.microsoft.com/en-us/office/vba/api/powerpoint.shapes.pastespecial
+    # Slide = presentation.Slides.Add(presentation.Slides.Count + 1, 12)
+    Slide = presentation.Slides.Item(1)
+    pchart = Slide.Shapes.PasteSpecial(11)
+    # Left, Top, Width, Height 로 지정 가능. pchart.Height = 500
+    pchart.Top = 400 * (count - 1)
 
     textbox = Slide.Shapes.AddTextbox(1, 100, 100, 200, 300)
     textbox.TextFrame.TextRange.Text = str(chart.Chart.ChartTitle.Text)
 
-presentation.SaveAs("C:\\Users\\Park\\Desktop\\private_project\\POC_chart_copy_to_ppt\\ppt_test.pptx")
-presentation.Close()
-pptApp.Quit()
+    count += 1
+
+# presentation.SaveAs(ppt_path)
+# presentation.Close()
+# pptApp.Quit()
 
 # CLOSE
 
